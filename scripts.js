98689f91015229589c71a1036f2f57a3ca49d187
patrickarcher414 <patrickarcher414@gmail.com>
2022-11-28 11:42:42 -0600

    turn flips color of tile

--- scripts.js
+++ scripts.js
@@ -1,8 +1,22 @@
 
+let turn = "white";
+
 const onClick = (event) => {
-  if (event.target.nodeName === "BUTTON") {
-    console.log(event.target.id);
+  if (event.target.nodeName != "BUTTON") {
+    return
   }
+
+  if (turn === "white") {
+    event.target.classList.remove('black')
+    event.target.classList.add('white')
+    turn = "black"
+  } else {
+    event.target.classList.remove('white')
+    event.target.classList.add('black')
+    turn = "white"
+  }
+
+  console.log(event.target.id)
 }
 
 window.addEventListener('click', onClick);
